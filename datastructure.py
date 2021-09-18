8cb736588a5c7f70681b29545dfc17cfe60f8837
MakiseJ <87430191+MakiseJ@users.noreply.github.com>
2021-09-18 17:18:20 +0800

    some slice points

---
+++ datastructure.py
@@ -0,0 +1,6 @@
+listtest=[   1,   4,   3,   2,   46,   6]
+print(listtest[4:2:-1])
+print(listtest[2:4:-1]) #nothing
+print(listtest[4:2:1]) #nothing!
+print(listtest[2:9:3])
+print(listtest[-1:-4:-1])
