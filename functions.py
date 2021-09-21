f0c590dedf60cff2c774089db3719ba00767160a
MakiseJ <joeychen29639@gmail.com>
2021-09-21 15:23:19 +0800

    test documentation

--- functions.py
+++ functions.py
@@ -16,11 +16,28 @@ def test_global():
 
 
 def some_function():
+
     pass
 
 
 def afuncwithparam(a, b=10, c=12):
     print(f"a={a},b={b},c={c}")
+
+
+def testTuple():
+    a=10
+    b=3
+    return a,b
+
+
+def testDoc():
+    """This is a comment
+
+    Try to print me:)."""
+    print("Hi!")
+    a=13
+    """This is another comment"""
+
 
 globalV = 33
 x=19
@@ -32,5 +49,8 @@ test_global()
 print("now globalV actually is", globalV)
 some_function()
 afuncwithparam(33,c=32)
-afuncwithparam(12, 2, b=33) # duplicated passing is invalid
+# afuncwithparam(12, 2, b=33) # duplicated passing is invalid
 # afuncwithparam(a=12,32) # cannot use keyword before positional parameter
+a,b=testTuple()
+print(f"now b is {b}")
+print(testDoc.__doc__)
